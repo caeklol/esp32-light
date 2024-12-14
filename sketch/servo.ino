790fafa5b558feff19556d8e2ef74ef3d99789aa
caeklol <me@caek.dev>
2024-12-14 16:24:03 +0800

    detach on unuse

--- sketch/servo.ino
+++ sketch/servo.ino
@@ -11,6 +11,8 @@ void ServoSync(void *pvParameters) {
   for (;;) {
     if (lastState != state) {
       Serial.println("Syncing state");
+      servo.attach(33);
+      delay(100);
       if (state) {
         servo.write(60);
       } else {
@@ -22,6 +24,7 @@ void ServoSync(void *pvParameters) {
       continue;
     } else {
       delay(250);
+      servo.detach(33);
     }
   }
 }
