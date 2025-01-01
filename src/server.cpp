b3abd50ffc6c51b4c0dcce9f97d5c6968ab060f2
caeklol <me@caek.dev>
2025-01-01 15:12:59 +0800

    minify

--- src/server.cpp
+++ src/server.cpp
@@ -17,10 +17,10 @@ String getPage(String content) {
   html += "<title>Light Debug</title>";
   html += "<link rel='preconnect' href='https://fonts.googleapis.com'>";
   html += "<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>";
-  html += "<link href='https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap' rel='stylesheet'>";
+  html += "<link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap' rel='stylesheet'>";
   html += "<style>";
-  html += "body { font-size: 1em; text-align:center; background-color: #11111b; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; color: #e9ecf5; font-family: Inter, sans-serif;}";
-  html += ".container { border-radius: 5px; padding: 20px; padding-right: 40px; padding-left: 40px; background-color: #1e1e2e; }";
+  html += "body {font-size:1em;text-align:center;background-color:#11111b;display:flex;justify-content:center;align-items:center;height:100vh;margin:0;color:#e9ecf5;font-family:Inter, sans-serif;}";
+  html += ".container {border-radius:5px;padding:20px;background-color:#1e1e2e;}";
   html += "</style>";
   html += "</head>";
   html += "<body><div class='container'>";
@@ -35,30 +35,24 @@ void handleHome() {
   content += "<div style='font-size:48px;animation:bounce 1s infinite;'>&#128075;</div>";
   content += "<div class='container'>";
   content += "<style>";
-  content += "button { background-color: #e5c890; color: #292c3c; border-radius: 5px; border-width: 0; padding: 5px; }";
-  content += "button: active { background-color: #f0c369; }";
-  content += ".green { color: #a6d189; }";
-  content += ".red { color: #e78284; }";
+  content += "button { background-color:#e5c890;color:#292c3c;border-radius:5px;border:none;padding:5px;}";
+  content += ".green{color: #a6d189;}";
+  content += ".red{color: #e78284;}";
   content += "</style>";
   content += "<h2>Light is currently</h2>";
-  content += "<p><strong";
+  content += "<p><strong ";
   content += state ? "class='green'>ON" : "class='red'>OFF";
   content += "</strong></p>";
   content += "<form action='/toggle' method='GET'><button type='submit'>Toggle the lights</button></form>";
   content += "</div>";
-  content += "<style>@keyframes bounce{0%,100%{transform:translateY(10px);}50%{transform:translateY(-10px);}}</style>";
+  content += "<style>@keyframes bounce {0%,100% {transform: translateY(10px);}50%{transform:translateY(-10px);}}</style>";
   server.send(200, "text/html", getPage(content));
 }
 
 void handleToggle() {
   state = !state;
-  String content = "<strong>successful! redirecting...</strong>";
-  content += "<br>";
-  content += "<br>";
-  content += "<br>";
-  content += "<i font-size: 0.1em;>id: ";
+  String content = "<i>successful! redirecting...</i>";
   content += "<meta http-equiv='refresh' content='0; url=/'>";
-  content += "</i>";
   
   server.send(200, "text/html", getPage(content));
 }
